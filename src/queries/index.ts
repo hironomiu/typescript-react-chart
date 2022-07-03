1ca8f55641ac966a73691d7935447dbe66d13ff4
hironomiu <hironomiu@gmail.com>
2022-07-03 15:10:42 +0900

    commited

--- src/queries/index.ts
+++ src/queries/index.ts
@@ -1,7 +1,6 @@
 import { useQuery } from 'react-query'
 
 export const getPrefectures = async ({ apiKey }: { apiKey: string }) => {
-  console.log('called a')
   const data = await fetch(
     'https://opendata.resas-portal.go.jp/api/v1/prefectures',
     {
@@ -12,14 +11,11 @@ export const getPrefectures = async ({ apiKey }: { apiKey: string }) => {
     }
   )
   const json = await data.json()
-  console.log('called b:', json)
   return json.result
 }
 
 export const usePrefectures = ({ apiKey }: { apiKey: string }) => {
-  console.log('called c')
-
-  const { data, refetch, error, status } = useQuery({
+  const { data, refetch } = useQuery({
     queryKey: 'prefectures',
     queryFn: async () => getPrefectures({ apiKey }),
     cacheTime: 30000,
@@ -27,8 +23,6 @@ export const usePrefectures = ({ apiKey }: { apiKey: string }) => {
     enabled: false,
   })
 
-  console.log('called d')
-  console.log(error)
   return { prefecturesData: data, prefecturesRefetch: refetch }
 }
 
@@ -59,14 +53,13 @@ export const usePopulationComposition = ({
   apiKey: string
   prefCode: string
 }) => {
-  const { data, refetch, error } = useQuery({
+  const { data, refetch } = useQuery({
     queryKey: 'populationComposition',
     queryFn: async () => getPopulationComposition({ apiKey, prefCode }),
     cacheTime: 30000,
     staleTime: 30000,
     enabled: false,
   })
-  console.log(error)
   return {
     populationCompositionData: data,
     populationCompositionRefetch: refetch,
